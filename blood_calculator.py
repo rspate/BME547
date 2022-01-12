8cb248ecbfa514d1c4f7d727dcb48cc1215dd7ed
Rachel Pate <rachel.pate@duke.edu>
2022-01-12 16:35:57 -0500

    Created generic output function, linked to HDL_driver

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,6 +12,10 @@ def accept_input(test_name):
 	entry = input("Enter the {} test result: ".format(test_name))
 	return int(entry)
 
+def print_result(test_name, test_value, test_class):
+	out_string = "The test value of {} for {} is {}".format(test_value, test_name, test_class)
+	print(out_string)
+	
 def check_HDL(HDL_value):
 	if HDL_value >= 60:
 		answer = "Normal"
@@ -24,8 +28,8 @@ def check_HDL(HDL_value):
 def HDL_driver():
 	HDL_value = accept_input("HDL")
 	classification = check_HDL(HDL_value)
+	print_result("HDL", HDL_value, classification)
 	
-	
-	
+
 	
 interface()
