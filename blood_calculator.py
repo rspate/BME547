da9df53ea176c88355083923a69a39df50c15cc8
Rachel Pate <rachel.pate@duke.edu>
2022-01-12 16:38:23 -0500

    Added HDL option to main interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,11 +3,15 @@ def interface():
 	keep_running = True
 	while keep_running:
 		print("Options:")
+		print("1-HDL")
 		print("9-Quit")
 		choice = input("Enter your choice: ")
 		if choice == "9":
 			keep_running = False
-
+		elif choice == "1":
+			HDL_driver()
+	return
+	
 def accept_input(test_name):
 	entry = input("Enter the {} test result: ".format(test_name))
 	return int(entry)
